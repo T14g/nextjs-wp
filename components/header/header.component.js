a408121903ee7fe48d8dec8000c28cd84705c2db
Tiago <ottiagos@gmail.com>
2021-11-05 00:02:24 -0300

    first commit

---
+++ components/header/header.component.js
@@ -0,0 +1,9 @@
+import React from "react";
+import HeaderStyles from "./header.styles";
+
+const Header = () => 
+    <HeaderStyles>
+        Logo | Link Link Link Link Link
+    </HeaderStyles>
+
+export default Header;
