47d08a7eaec354417fdb65d358e2b9ada4a6749f
Tiago <ottiagos@gmail.com>
2021-11-14 22:39:36 -0300

    Typewritter looping

--- components/typewritter/typewritter.component.js
+++ components/typewritter/typewritter.component.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import TypeWritterStyles, { TypingText, StaticText, ContentBlock } from "./typewritter.styles";
-const words = ['Developer', 'Magician'];
+const words = ['Developer', 'Magician', 'Guitar Hero'];
 
 const TypeWritter = () => {
 
@@ -23,20 +23,33 @@ const TypeWritter = () => {
 
     // typeWriter
     useEffect(() => {
-        if (index === words.length) return;
 
+        // If you reach the end don't do anything
+        if (index === words.length -1 && subIndex == words[index].length + 1) {
+            setIndex(0);
+            setSubIndex(0);
+            setblink(true);
+            setReverse(false);
+        };
+   
+
+        // On the last index of word and NOT on last word NOT reversing
+        // Do the reverse thing so
         if (subIndex === words[index].length + 1 &&
             index !== words.length - 1 && !reverse) {
             setReverse(true);
             return;
         }
-
+        
+        // 1st index and still reversing? stop reverse and go to index 1
         if (subIndex === 0 && reverse) {
             setReverse(false);
             setIndex((prev) => prev + 1);
             return;
         }
 
+        // If you wont returned the above cases
+        // 
         const timeout = setTimeout(() => {
             setSubIndex((prev) => prev + (reverse ? -1 : 1));
         }, Math.max(reverse ? 75 : subIndex === words[index].length ? 1000 :
@@ -51,7 +64,7 @@ const TypeWritter = () => {
             <ContentBlock className="contentBlock">
                 <StaticText>Hello I'm T14g</StaticText>
                 <TypingText>
-                The {`${words[index].substring(0, subIndex)}${blink ? "|" : ""}`}
+                    The {`${words[index].substring(0, subIndex)}${blink ? "|" : ""}`}
                 </TypingText>
             </ContentBlock>
         </TypeWritterStyles>
