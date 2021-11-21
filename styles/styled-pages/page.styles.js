37621ecd879acb38977ea2843a10073aa5b1f72f
Tiago <ottiagos@gmail.com>
2021-11-20 23:53:10 -0300

    Page Column Styled

--- styles/styled-pages/page.styles.js
+++ styles/styled-pages/page.styles.js
@@ -13,11 +13,35 @@ export const FlexRow = styled.div`
 `;
 
 export const Column = styled.div`
+  padding: 0 15px;
+
   &.col-6{
     width: 50%;
   }
 `;
 
+
 export const TitleH1 = styled.h1`
   text-align: center;
+  font-size: 50px;
+`;
+
+export const TitleH2 = styled.h2`
+  font-size: 40px;
+
+  &.centered{
+    text-align: center;
+  }
+`;
+
+export const TextBlock = styled.p`
+  font-size: 20px;
+
+  &.centered{
+    text-align: center;
+  }
+
+  @media(max-width: 1230px) {
+    font-size: 18px;
+  }
 `;
