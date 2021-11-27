35735f53da7d6d8d2de182275a94c4ba99c6c746
Tiago <ottiagos@gmail.com>
2021-11-26 23:51:49 -0300

    Big title js into Ts

--- components/big-title/big-title.component.tsx
+++ components/big-title/big-title.component.tsx
@@ -1,7 +1,12 @@
 import React from "react";
 import BigTitleStyles from "./big-title.styles";
 
-const BigTitle = ({ children, className }) => {
+interface BigTitleProps {
+    children: any,
+    className: string
+};
+
+const BigTitle = ({ children, className }: BigTitleProps) => {
     return (
         <BigTitleStyles className={className}>{children}</BigTitleStyles>
     )
