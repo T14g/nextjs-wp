8b788a0435869631e9f8b7558ecb24af436d69a1
Tiago <ottiagos@gmail.com>
2021-12-06 23:38:10 -0300

    Showcase component bones

---
+++ components/showcase/showcase.component..tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import ShowcaseStyles from './showcase.styles';
+
+const Showcase = ({ list }) => {
+    return (
+        <ShowcaseStyles>Showcase componet</ShowcaseStyles>
+    )
+};
+
+export default Showcase;
---
+++ components/showcase/showcase.styles.ts
@@ -0,0 +1,7 @@
+import styled from 'styled-components';
+
+const ShowcaseStyles = styled.div`
+
+`;
+
+export default ShowcaseStyles;
