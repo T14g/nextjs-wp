022d6a642ee3a9d7f92dbf4a6b282fd9e643c389
Tiago <ottiagos@gmail.com>
2022-07-25 00:14:31 -0300

    arrow-button is now round

--- components/arrow-button/arrow-button.styles.js
+++ components/arrow-button/arrow-button.styles.js
@@ -4,8 +4,11 @@ const ArrowButtonStyles = styled.button`
     background: #000;
     color: #fff;
     padding: 10px;
-    font-size: 25px;
+    font-size: 30px;
     border: none;
+    border-radius: 50%;
+    height: 60px;
+    width: 60px;
 `;
 
 export default ArrowButtonStyles;
