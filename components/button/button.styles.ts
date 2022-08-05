3f5016f324f102a107f4ceb4c26e0f8e475148b0
Tiago <ottiagos@gmail.com>
2022-08-04 23:42:44 -0300

    yellow button

--- components/button/button.styles.ts
+++ components/button/button.styles.ts
@@ -2,16 +2,22 @@ import styled from "styled-components";
 import Link from 'next/link'
 
 const ButtonStyles = styled.button`
-    background: none;
-    border: 2px #000 solid;
-    padding: 7px 12px;
-    font-weight: 600;
+    display: block;
+    max-width: 363px;
+    width: 100%;
+    padding: 18px 13px;
+    background-color: #FFD900 !important;
+    border: none !important;
+    color: #000 !important;
+    font-size: 23px !important;
+    font-family: "Barlow Condensed", sans-serif;
+    border-radius: 55px !important;
+    font-weight: 900 !important;
+    margin-top: 20px;
 
     &:hover{
         cursor: pointer;
-        background: #000;
-        color: #fff;
-        transition: 0.8s;
+        opacity: 0.8;
     }
 `;
 
